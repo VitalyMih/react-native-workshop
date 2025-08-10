3d3551737468926072bc63f65cc2fd75aa6a4b8b
vmikhalevskii <vmikhalevskii@ronasit.com>
2025-08-10 15:43:00 +0300

    feat: use expo-app generator

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,50 @@
+import { EASConfig, ExpoConfig } from '@expo/config';
+
+const createConfig = (): Omit<ExpoConfig, 'extra'> & { extra: { eas: EASConfig } & typeof extra } => {
+  const projectId = '';
+
+  const appId = 'com.rn.workshop.dev';
+
+  const extra = {
+    eas: { projectId } as EASConfig,
+  };
+
+  return {
+    name: 'React Native Workshop Dev',
+    slug: 'react-native-workshop-app',
+    scheme: 'react-native-workshop-dev',
+    version: '0.1.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      url: `https://u.expo.dev/${projectId}`,
+    },
+    ios: {
+      bundleIdentifier: appId,
+      supportsTablet: false,
+      buildNumber: '1',
+      config: {
+        usesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      package: appId,
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+      edgeToEdgeEnabled: true,
+    },
+    plugins: ['expo-router', 'expo-localization'],
+    newArchEnabled: true,
+    extra,
+  };
+};
+
+export default createConfig;
--- libs/mobile/shared/data-access/storage/eslint.config.cjs
+++ libs/mobile/shared/data-access/storage/eslint.config.cjs
@@ -1,15 +1,12 @@
-import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+const nx = require('@nx/eslint-plugin');
+const baseConfig = require('../../../../../eslint.config.cjs');
 
-export default [
+module.exports = [
   ...baseConfig,
   ...nx.configs['flat/react'],
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
     rules: {},
-  },
-  {
-    ignores: ['.expo', 'web-build', 'cache', 'dist'],
   },
 ];
