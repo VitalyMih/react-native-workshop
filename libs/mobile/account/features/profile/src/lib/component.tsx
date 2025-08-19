18e47905f6c7c2891a9c94b6475126ff01634817
vmikhalevskii <vmikhalevskii@ronasit.com>
2025-08-19 18:07:49 +0300

    feat: add AppActionsBottomSheet component to ui-kit

--- libs/mobile/account/features/profile/src/lib/component.tsx
+++ libs/mobile/account/features/profile/src/lib/component.tsx
@@ -1,21 +1,39 @@
-import { ReactElement } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { useTranslation } from '@ronas-it/react-native-common-modules/i18n';
+import { ReactElement, useRef } from 'react';
 import { View, Text } from 'react-native';
 import { colors, createStyles, spacings } from '@react-native-workshop/mobile/shared/ui/styles';
-import { AppAvatar, AvatarSize } from '@react-native-workshop/mobile/shared/ui/ui-kit';
+import {
+  AppActionsBottomSheet,
+  AppAvatar,
+  AppBottomSheetAction,
+  AvatarSize,
+} from '@react-native-workshop/mobile/shared/ui/ui-kit';
 import { AlertService } from '@react-native-workshop/mobile/shared/utils/alert-service';
 
 export function Profile(): ReactElement {
-  const onEditAvatarPress = (): void => AlertService.showUnderConstruction();
+  const translate = useTranslation('ACCOUNT.PROFILE');
+  const actionsRef = useRef<BottomSheetModal>(null);
+
+  const showUnderConstruction = (): void => AlertService.showUnderConstruction();
+
+  const openActions = (): void => actionsRef.current?.present();
+
+  const actions: Array<AppBottomSheetAction> = [
+    { title: translate('BUTTON_CHANGE_PHOTO'), iconName: 'edit', onPress: showUnderConstruction },
+    { title: translate('BUTTON_DELETE_PHOTO'), iconName: 'trash', onPress: showUnderConstruction },
+  ];
 
   return (
     <View style={styles.container}>
       <View style={styles.avatarContainer}>
-        <AppAvatar size={AvatarSize.LARGE} badgeIconName='edit' onPress={onEditAvatarPress} />
+        <AppAvatar size={AvatarSize.LARGE} badgeIconName='edit' onPress={openActions} />
         <View style={styles.infoContainer}>
           <Text style={styles.name}>{'Johnathan Johnson'}</Text>
           <Text style={styles.email}>{'john123@gmail.com'}</Text>
         </View>
       </View>
+      <AppActionsBottomSheet ref={actionsRef} actions={actions} />
     </View>
   );
 }
---
+++ libs/mobile/shared/ui/ui-kit/src/lib/actions-bottom-sheet/component.tsx
@@ -0,0 +1,84 @@
+import BottomSheet, { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { useTranslation } from '@ronas-it/react-native-common-modules/i18n';
+import { AppSafeAreaView } from '@ronas-it/react-native-common-modules/safe-area-view';
+import { ForwardedRef, ReactElement, RefObject } from 'react';
+import { Text, View } from 'react-native';
+import { colors, createStyles, spacings } from '@react-native-workshop/mobile/shared/ui/styles';
+import { AppBottomSheet, AppBottomSheetProps } from '../bottom-sheet';
+import { Icon } from '../icon';
+import { AppPressable } from '../pressable';
+import { AppBottomSheetAction } from './types';
+
+export interface AppActionsBottomSheetProps extends Omit<AppBottomSheetProps, 'children'> {
+  actions: Array<AppBottomSheetAction>;
+  ref?: ForwardedRef<BottomSheetModal | BottomSheet>;
+}
+
+export function AppActionsBottomSheet({ actions, ref, ...props }: AppActionsBottomSheetProps): ReactElement {
+  const translate = useTranslation('SHARED.APP_ACTIONS_BOTTOM_SHEET');
+
+  const closeModal = (): void => (ref as RefObject<BottomSheetModal | BottomSheet>)?.current.close();
+
+  const cancelAction = {
+    title: translate('BUTTON_CANCEL'),
+    onPress: closeModal,
+  };
+
+  const renderActionRow = (action: AppBottomSheetAction): ReactElement => {
+    const isCancelAction = action === cancelAction;
+
+    return (
+      <AppPressable
+        style={[styles.actionContainer, isCancelAction && styles.cancelAction]}
+        onPress={action.onPress}
+        key={action.title}>
+        {action.iconName && <Icon name={action.iconName} />}
+        <Text style={styles.text}>{action.title}</Text>
+      </AppPressable>
+    );
+  };
+
+  return (
+    <AppBottomSheet
+      ref={ref}
+      style={styles.container}
+      isModal
+      hasBackdrop
+      headerless
+      withoutBackground
+      enablePanDownToClose={false}
+      {...props}>
+      <AppSafeAreaView edges={['bottom']}>
+        <View style={styles.actionsContainer}>{actions.map(renderActionRow)}</View>
+        {renderActionRow(cancelAction)}
+      </AppSafeAreaView>
+    </AppBottomSheet>
+  );
+}
+
+const styles = createStyles({
+  container: {
+    paddingHorizontal: spacings.basicOffset,
+  },
+  actionsContainer: {
+    overflow: 'hidden',
+    borderRadius: spacings.secondaryBorderRadius,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '0.625rem',
+    paddingVertical: spacings.contentOffset,
+    paddingHorizontal: spacings.containerOffset,
+    backgroundColor: colors.backgroundTertiary,
+  },
+  cancelAction: {
+    marginTop: spacings.contentOffset,
+    borderRadius: spacings.secondaryBorderRadius,
+    justifyContent: 'center',
+  },
+  text: {
+    color: colors.textPrimary,
+    fontSize: 16,
+  },
+});
